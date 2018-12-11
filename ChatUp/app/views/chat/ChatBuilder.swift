36034dfb415353b7c24496ad0c344550b9511ad7
Mark Alain D. Rufino <mark.rufino.io@gmail.com>
2018-12-11 15:37:16 +0800

    add login page view controller

--- ChatUp/app/views/chat/ChatBuilder.swift
+++ ChatUp/app/views/chat/ChatBuilder.swift
@@ -14,8 +14,7 @@ class ChatBuilder: BuilderType<ChatViewController> {
 		let userInfoService = UserInfoService.default
 		let credentials = PusherKeys.default
 		view.userInfoService = userInfoService
-		view.chatService = MockChatService(serviceable: view)
-//		view.chatService = ChatService(Provider.default, withUserInfoService: userInfoService, andCredentials: credentials, toService: view)
+		view.chatService = ChatService(Provider.default, withUserInfoService: userInfoService, andCredentials: credentials, toService: view)
         return view
     }
     
